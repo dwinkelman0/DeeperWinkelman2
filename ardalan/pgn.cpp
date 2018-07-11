825ad4a30074cb61f6fb27f4a14cee079457f196
Daniel Winkelman <dwinkelman3@gmail.com>
2018-07-10 21:34:21 -0400

    Finished PGN implementation

--- ardalan/pgn.cpp
+++ ardalan/pgn.cpp
@@ -6,35 +6,112 @@
 bool Board::MakePGNMoves(const char * pgn) {
 	
 	bool white_to_move = true;
-	uint8_t start_piece = 0;
-	uint8_t end_square = 0;
 	
 	// Delimit string on whitespace
 	char * copy = strdup(pgn);
 	const char * pch = strtok(copy, " \n\t");
 	while (pch) {
-		// Check that not a move number...
-		if (pch[0] >= '0' && pch[0] <= '9') goto next;
+		// Check that not a move number or a game termination
+		if ((pch[0] >= '0' && pch[0] <= '9') || pch[0] == '*') {
+			pch = strtok(NULL, " \n\t");
+			continue;
+		}
 		
-		// ...or a game termination
-		if (pch[0] == '*') goto next;
-		
-		// Get piece type
-		switch (pch[0]) {
-			case 'K': start_piece = white_to_move ? WHITE_KING : BLACK_KING; break;
-			case 'Q': start_piece = white_to_move ? WHITE_QUEEN : BLACK_QUEEN; break;
-			case 'R': start_piece = white_to_move ? WHITE_ROOK : BLACK_ROOK; break;
-			case 'B': start_piece = white_to_move ? WHITE_BISHOP : BLACK_BISHOP; break;
-			case 'N': start_piece = white_to_move ? WHITE_KNIGHT : BLACK_KNIGHT; break;
-			default:  start_piece = white_to_move ? WHITE_PAWN : BLACK_PAWN; break;
+		// Check for castling; if found, do it
+		if (!strcmp(pch, "O-O") || !strcmp(pch, "O-O-O")) {
+			Move move;
+			if (!strcmp(pch, "O-O")) {
+				move = Move(0, 0, white_to_move ? Move::WHITE_OO : Move::BLACK_OO);
+			}
+			else {
+				move = Move(0, 0, white_to_move ? Move::WHITE_OOO : Move::BLACK_OOO);
+			}
+			if (!Make(move)) {
+				return false;
+			}
+		}
+		else {
+			uint8_t start_piece = 0;
+			uint8_t end_square = 0;
+			int n_row = 0, n_col = 0, n_capture = 0;
+			const char * first_pos = NULL;
+			
+			// Get piece type
+			switch (pch[0]) {
+				case 'K': start_piece = white_to_move ? WHITE_KING : BLACK_KING; break;
+				case 'Q': start_piece = white_to_move ? WHITE_QUEEN : BLACK_QUEEN; break;
+				case 'R': start_piece = white_to_move ? WHITE_ROOK : BLACK_ROOK; break;
+				case 'B': start_piece = white_to_move ? WHITE_BISHOP : BLACK_BISHOP; break;
+				case 'N': start_piece = white_to_move ? WHITE_KNIGHT : BLACK_KNIGHT; break;
+				default:  start_piece = white_to_move ? WHITE_PAWN : BLACK_PAWN; break;
+			}
+			
+			// Scan for 'x' and how many coordinate characters
+			const char * i = pch;
+			while (*i) {
+				if (*i >= '1' && *i <= '8') {
+					n_row++;
+					if (!first_pos) first_pos = i;
+				}
+				else if (*i >= 'a' && *i <= 'h') {
+					n_col++;
+					if (!first_pos) first_pos = i;
+				}
+				else if (*i == 'x') {
+					n_capture++;
+					if (!first_pos) first_pos = i;
+				}
+				i++;
+			}
+			int n_pos_char = n_row + n_col + n_capture;
+			
+			// Get ending coordinate
+			end_square = Text2Coord(first_pos - 2 + n_pos_char);
+			
+			// Get moves to find which one matches the constraints
+			bool any_moves = false;
+			BoardState state = GetCurrent();
+			const MoveList * moves = GetMoves();
+			const Move * move_i = moves->Begin();
+			const Move * move_end = moves->End();
+			for (; move_i != move_end; move_i++) {
+				// Make sure start piece matches
+				if (state.squares[move_i->start] != start_piece) continue;
+				
+				// Make sure end square matches
+				if (move_i->end != end_square) continue;
+				
+				// If there are further constraints, check those
+				if (n_row == 2 && n_col == 2) {
+					uint8_t start_square = Text2Coord(first_pos);
+					if (move_i->start != start_square) continue;
+				}
+				else if (n_row == 2) {
+					if (move_i->start / 8 != *first_pos - '1') continue;
+				}
+				else if (n_col == 2) {
+					if (move_i->start % 8 != *first_pos - 'a') continue;
+				}
+				
+				// Passed all the tests, make the move
+				if (Make(*move_i)) {
+					// Make sure move is legal
+					if (InCheck(state.white_to_move)) {
+						Unmake(1);
+					}
+					else {
+						any_moves = true;
+						break;
+					}
+				}
+				else return false;
+			}
+			
+			if (!any_moves) return false;
 		}
 		
 		white_to_move = !white_to_move;
-		std::cout << "Parsed " << (int)start_piece << ": " << pch << std::endl;
-		
-		next: {
-			pch = strtok(NULL, " \n\t");
-		}
+		pch = strtok(NULL, " \n\t");
 	}
 	
 	free(copy);
--- ardalan_tests/tests.cpp
+++ ardalan_tests/tests.cpp
@@ -169,7 +169,16 @@ void Test_UnmoveGeneration() {
 }
 
 void Test_PGN() {
-	const char * game = " \
+	const char * game = "1. e4 d6 2. Bb5+ Nd7 3. d4 Nf6";
+	/*
+	    1. e4 e5 \
+		2. Nf3 Nf6 \
+		3. d4 exd4 \
+		4. e5 Ne4 \
+		5. Qxd4 d5 \
+		6. exd6";
+	*/
+	/*
 		1. e4 e5 2. Nf3 Nc6 3. Bb5 a6 4. Ba4 Nf6 5. O-O Be7 6. Re1 b5 7. Bb3 d6 8. c3 \
 		O-O 9. h3 Nb8 10. d4 Nbd7 11. c4 c6 12. cxb5 axb5 13. Nc3 Bb7 14. Bg5 b4 15. \
 		Nb1 h6 16. Bh4 c5 17. dxe5 Nxe4 18. Bxe7 Qxe7 19. exd6 Qf6 20. Nbd2 Nxd6 21. \
@@ -177,7 +186,8 @@ void Test_PGN() {
 		27. Qe3 Qg5 28. Qxg5 hxg5 29. b3 Ke6 30. a3 Kd6 31. axb4 cxb4 32. Ra5 Nd5 33. \
 		f3 Bc8 34. Kf2 Bf5 35. Ra7 g6 36. Ra6+ Kc5 37. Ke1 Nf4 38. g3 Nxh3 39. Kd2 Kb5 \
 		40. Rd6 Kc5 41. Ra6 Nf2 42. g4 Bd3 43. Re6 1/2-1/2";
-		
+	*/
 	Board board;
+	board.SetCurrent(BoardState());
 	board.MakePGNMoves(game);
 }
